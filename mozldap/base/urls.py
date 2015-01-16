f39fd7f2ddf7678cff4c6302ae9e3c4efc162832
Peter Bengtsson <mail@peterbe.com>
2015-01-16 13:16:56 -0800

    upgrade to django 1.6

--- mozldap/base/urls.py
+++ mozldap/base/urls.py
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from . import views
 
 
--- mozldap/urls.py
+++ mozldap/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls import patterns, include
 
 from .base import urls as base_urls
 from .docs import urls as docs_urls
